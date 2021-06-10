ae109780d71da4d2af1828ca145fa41452358479
setiawanswan <setiawan.tumilin@gmail.com>
2021-06-10 08:32:59 +0700

    state mutation actions Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ message + ' Saya lagi belajar VueX' }}
+    <input type="text" v-model="newMessage">
+    <button @click="update">Update</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapMutations, mapActions } from 'vuex'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      newMessage : ''
+    }
+  },
+
+  computed: {
+    ...mapState(['message'])
+  },
+
+  methods : {
+    ...mapActions(['updateWithNewMessage']),
+    update(){
+      this.updateWithNewMessage( this.newMessage)
+    }
   }
+
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    message : 'Hello Vuex'
   },
   mutations: {
+    updateMessage(state, value){
+      state.message = value
+    }
   },
   actions: {
+    updateWithNewMessage( {commit }, value){
+      commit('updateMessage', value)
+    }
   },
   modules: {
   }
